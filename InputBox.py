72b3e994a8253276f6f2342df2c6c80026cfff53
yuru <d0542900@mail.fcu.edu.tw>
2019-05-21 12:11:47 +0800

    no message

--- InputBox.py
+++ InputBox.py
@@ -54,34 +54,3 @@ class InputBox:
         screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y))
         # Blit the rect.
         pg.draw.rect(screen, self.color, self.rect, 2)
-
-
-
-def main():
-    clock = pg.time.Clock()
-    input_box1 = InputBox(100, 100, 140, 32)
-    input_box2 = InputBox(100, 300, 140, 32)
-    input_boxes = [input_box1, input_box2]
-    done = False
-
-    while not done:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                done = True
-            for box in input_boxes:
-                box.handle_event(event)
-
-        for box in input_boxes:
-            box.update()
-
-        screen.fill((30, 30, 30))
-        for box in input_boxes:
-            box.draw(screen)
-
-        pg.display.flip()
-        clock.tick(30)
-
-
-if __name__ == '__main__':
-    main()
-    pg.quit()
